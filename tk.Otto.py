afdb9b75d13fd2fc80525a396388702bfcd5ffbc
ottovic <otto.szentjobi@gmail.com>
2022-09-15 14:18:24 +0200

    Update tk.Otto.py

--- tk.Otto.py
+++ tk.Otto.py
@@ -1,3 +1,4 @@
+
 from tkinter import *
 from tkinter import messagebox
 import tkinter as tk
@@ -5,27 +6,35 @@ from tkinterweb import HtmlFrame
 
 def doSomething():
 #    messagebox.showinfo("I am doing something")
+    global htmlText
     print("I am doing something")
+    htmlText = htmlText + "<br>I am doing something"
+    displayHtml()
 
+def displayHtml():
+    global htmlText
+    global root
+    global myhtmlframe
+    print ("diplaying")
+    myhtmlframe.load_html(htmlText) #Load some HTML code
+    myhtmlframe.pack(fill="both", expand=True) #attach the HtmlFrame widget to the parent window
+    button = Button(text="PressMe",command=doSomething)
+    button.place(x=500, y=50)
+    root.mainloop()
 
 root = tk.Tk()
 root.geometry("700x400")
+htmlText = "<h1>Hello, World!</h1>"
 # Label = tk.Label(root, text="Populáció genetikai szimuláció")
 # Label.pack()
 root.title('Populáció genetikai szimuláció')
 root.resizable(width=True, height=True)
 
 myhtmlframe = HtmlFrame(root) #create HTML browser
-myhtmlframe.load_html("<h1>Hello, World!</h1>") #Load some HTML code
-myhtmlframe.pack(fill="both", expand=True) #attach the HtmlFrame widget to the parent window
 
-root.bind("PRESS ME", doSomething)
-
-button = Button(text="PressMe",command=doSomething)
-button.place(x=115, y=250)
+#button = Button(text="PressMe",command=doSomething)
+#button.place(x=500, y=50)
 
 print("DONE")
 
-root.mainloop()
-
-
+displayHtml()
